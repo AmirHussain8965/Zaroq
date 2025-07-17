9bfb997751e60fa0faa756ae2ed57bf193174118
Amir Hussain <amirhussain8965@gmail.com>
2025-07-17 10:33:04 +0500

    first commit

---
+++ assets/js/script2.js
@@ -0,0 +1,270 @@
+const translations = {
+    ru: {
+        header1: "Услуги",
+        header2: "Действие",
+        header3: "Другой",
+        header4: "Что-нибудь",
+        header5: "О нас",
+        header6: "Тематические исследования",
+        header7: "Блог",
+        header8: "Карьера",
+        header9: "Связаться с нами",
+        // Hero_Section-------------
+        hero1: "Британское качество.",
+        ab_hero1: "Мы строим то, что обещаем — с точностью Соединенного Королевства",
+        hero2: "Теперь в Казахстане.",
+        hero3: "Zaroq — это зарегистрированное в Великобритании цифровое агентство с командой разработчиков, полностью базирующейся в Великобритании. Мы создаем веб-сайты и мобильные приложения мирового класса — теперь доступные для предприятий и стартапов в Казахстане.",
+        hero4: "Запланировать звонок",
+        hero5: "Получить предложение",
+        // Section_one-------------
+        sec1_txt1: "Почему стоит работать с нами?",
+        sec1_txt2: "Мы объединяем британский опыт и понимание местного рынка, чтобы воплотить ваши цифровые идеи в жизнь — правильно и с первого раза.",
+        sec1_txt3: "Соединенное Королевство уровень качества",
+        sec1_txt4: "современный дизайн",
+        sec1_txt5: "свободное общение на английском языке",
+        sec1_txt6: "Быстрая, прозрачная и надежная доставка",
+        // What_build_section---------
+        sec1_txt7: "Что мы строим?",
+        sec1_txt8: "UI/UX Дизайн",
+        sec1_txt9: "Веб-разработка",
+        sec1_txt10: "MVPS для стартапов",
+        sec1_txt11: "Разработка приложений",
+        // Home_Section_Two------------
+        sec2_txt1: "Как это работает?",
+        sec2_txt2: "01 Забронировать звонок",
+        sec2_txt3: "Давайте поговорим о ваших целях и видении — без каких-либо условий.",
+        sec2_txt4: "02 Получите подробное предложение",
+        sec2_txt5: "Четкое ценообразование, сроки и техническая разбивка. Никаких сюрпризов.",
+        sec2_txt6: "03 Дизайн и разработка",
+        sec2_txt7: "Мы воплощаем ваш продукт в жизнь — с помощью постоянных обновлений и сотрудничества.",
+        sec2_txt8: "04 Запуск и поддержка",
+        sec2_txt9: "Включено полное развертывание, тестирование и поддержка после запуска.",
+        sec2_txt10: "Запланировать звонок",
+        // Home_Section_Three ------------
+        sec3_txt1: "Вся команда разработчиков Zaroq базируется в Великобритании. Это означает высочайшие стандарты разработки, проверенные рабочие процессы и стабильные результаты — предоставляемые клиентам в Казахстане и за его пределами. Вы получаете экспертное обслуживание без проблем с недопониманием или пропущенными сроками.",
+        sec3_txt2: "Великобритания - Базовая команда. Глобальное ышление.",
+        // Clients_Section---------
+        cli_txt1: "Что говорят наши клиенты",
+        cli_txt2: "“Zaroq помог нам запустить нашу платформу быстрее, чем мы себе представляли, — без каких-либо компромиссов в дизайне”",
+        cli_txt3: "— Основатель стартапа, Лондон",
+        cli_txt4: "“Команда была профессиональной, отзывчивой и предоставила именно то, что нам было нужно.”",
+        cli_txt5: "— Генеральный директор, Казахстан",
+        // Project_Section---------
+        pro_txt1: "Давайте поговорим о вашем проекте",
+        pro_txt2: "Забронируйте бесплатную консультацию и свяжитесь напрямую с нашими ведущими разработчиками или дизайнерами. Мы выслушаем, посоветуем и поможем вам сделать первый шаг к созданию чего-то великого.",
+        pro_txt3: "Запланировать звонок",
+        // footer_Section---------
+        ftr_txt1: "Британское качество, казахские возможности",
+        ftr_txt2: "Контакт",
+        ftr_txt3: "2118 Thornridge Cir. Сиракьюс, Коннектикут 35624",
+        ftr_txt4: "Посмотрите наше местоположение на карте",
+        ftr_txt5: "Языки",
+        ftr_txt6: "политика конфиденциальности",
+        ftr_txt7: "Декларация о файлах cookie",
+        ftr_txt8: "мы преодолеваем разрыв между смелыми идеями и их цифровой реализацией мирового класса.",
+        ftr_txt9: "Условия эксплуатации",
+        ftr_txt10: "Контакт",
+        ftr_txt11: "© Zaroq 2025. Все права защищены.",
+        // WMD_Section ===========
+        wmd_txt1: "Что делает нас другими",
+        wmd_txt2: "Никакого аутсорсинга",
+        wmd_txt3: "Никаких размытых сроков",
+        wmd_txt4: "Никакой “шаблонной” работы",
+        wmd_txt5: "разработка мирового уровня",
+        // story_Section---------
+        stry_txt1: "Біздің әңгіме",
+        stry_txt2: "Zaroq Ұлыбританияда бір миссиямен құрылды:",
+        stry_txt3: "әлемдік деңгейдегі цифрлық өнімдерді ымырасыз жеткізіңіз.",
+        stry_txt4: "Біздің команданың әрбір мүшесі Біріккен Корольдікте орналасқан және біз қазір бүкіл Қазақстан бойынша өршіл стартаптар мен бизнеске өз қызметтерімізді ұсынуды мақтан тұтамыз.",
+        // Our_Missions_Section ===========
+        // msn_txt1: "Наша миссия",
+        // msn_txt2: "Предоставить казахстанскому бизнесу приложения и веб-сайты мирового уровня, подкрепленные надежной связью и доставкой.",
+
+
+
+
+        Example: "Русский",
+    },
+    kk: {
+        header1: "Қызметтер",
+        header2: "Әрекет",
+        header3: "Басқа",
+        header4: "Кез келген нәрсе",
+        header5: "Біз туралы",
+        header6: "Кейс зерттеулері",
+        header7: "Блог",
+        header8: "Мансап",
+        header9: "Бізбен хабарласыңы",
+        // Hero_Section-------------
+        hero1: "Британдық сапа.",
+        ab_hero1: "Біз уәде еткен нәрсені - Ұлыбританияның дәлдігімен жасаймыз",
+        hero2: "Қазір Қазақстанда.",
+        hero3: "Zaroq – Ұлыбританияда тіркелген, әзірлеу тобы бар, толығымен Ұлыбританияда орналасқан цифрлық агенттік. Біз әлемдік деңгейдегі веб-сайттар мен мобильді қосымшаларды жасаймыз – енді Қазақстандағы бизнес пен стартаптар үшін қолжетімді.",
+        hero4: "Қоңырауды жоспарлаңыз",
+        hero5: "Ұсыныс алыңыз",
+        // Section_one-------------
+        sec1_txt1: "Неліктен бізбен жұмыс істеуге тұрарлық?",
+        sec1_txt2: "Біз сіздің цифрлық идеяларыңызды бірінші рет жүзеге асыру үшін Ұлыбритания тәжірибесі мен жергілікті нарық туралы түсінікті біріктіреміз.",
+        sec1_txt3: "Ұлыбритания сапа деңгейі",
+        sec1_txt4: "заманауи дизайн",
+        sec1_txt5: "ағылшын тілінде еркін сөйлесу",
+        sec1_txt6: "Жылдам, мөлдір және сенімді жеткізу",
+        // What_build_section---------
+        sec1_txt7: "Біз не салып жатырмыз?",
+        sec1_txt8: "UI/UX дизайны",
+        sec1_txt9: "Веб әзірлеу",
+        sec1_txt10: "Стартаптарға арналған MVPS",
+        sec1_txt11: "Қолданбаны әзірлеу",
+        // Home_Section_Two------------
+        sec2_txt1: "Бұл қалай жұмыс істейді?",
+        sec2_txt2: "01 Қоңырауды брондау",
+        sec2_txt3: "Сіздің мақсаттарыңыз бен көзқарастарыңыз туралы сөйлесейік - ешқандай жолдар жоқ.",
+        sec2_txt4: "02 Толық ұсынысты алыңыз",
+        sec2_txt5: "Бағаларды, мерзімдерді және техникалық бұзылыстарды анық көрсетіңіз.Таңқаларлық емес",
+        sec2_txt6: "03 Жобалау және әзірлеу",
+        sec2_txt7: "Біз сіздің өніміңізді үнемі жаңартулар мен ынтымақтастық арқылы жүзеге асырамыз.",
+        sec2_txt8: "04 Іске қосу және қолдау",
+        sec2_txt9: "Толық орналастыру, тестілеу және іске қосудан кейінгі қолдау қамтылған.",
+        sec2_txt10: "Қоңырауды жоспарлаңыз",
+        // Home_Section_Three ------------
+        sec3_txt1: "Zaroq компаниясының бүкіл әзірлеу тобы Ұлыбританияда орналасқан. Бұл Қазақстандағы және одан тысқары елдердегі клиенттерге жеткізілетін ең жоғары даму стандарттарын, дәлелденген жұмыс үрдістерін және дәйекті нәтижелерді білдіреді. Сіз қателесусіз немесе өткізіп алған мерзімдерсіз сараптамалық қызмет аласыз.",
+        sec3_txt2: "Ұлыбритания - Негізгі команда. Жаһандық ойлау.",
+        // Clients_Section---------
+        cli_txt1: "Біздің клиенттер не дейді",
+        cli_txt2: "“Zaroq бізге платформамызды біз ойлағаннан тезірек іске қосуға көмектесті - дизайнда ешқандай ымырасыз.”",
+        cli_txt3: "— Стартаптың негізін қалаушы, Лондон",
+        cli_txt4: "“Команда кәсіби болды, жауап берді және бізге қажет нәрсені қамтамасыз етті.”",
+        cli_txt5: "— Бас директор, Қазақстан",
+        // Project_Section---------
+        pro_txt1: "Сіздің жобаңыз туралы сөйлесейік",
+        pro_txt2: "Тегін кеңеске тапсырыс беріңіз және жетекші әзірлеушілермен немесе дизайнерлермен тікелей байланысыңыз. Біз сізді тыңдап, кеңес береміз және керемет нәрсе жасауға алғашқы қадам жасауға көмектесеміз.",
+        pro_txt3: "Қоңырауды жоспарлаңыз",
+        // footer_Section---------
+        ftr_txt1: "Британдық сапа, қазақстандық мүмкіндіктер",
+        ftr_txt2: "Байланыс",
+        ftr_txt3: "2118 Торнридж Cir. Сиракуза, CT 35624",
+        ftr_txt4: "Біздің орналасқан жерімізді картадан қараңыз",
+        ftr_txt5: "Тілдер",
+        ftr_txt6: "Құпиялылық саясаты",
+        ftr_txt7: "Cookie декларациясы",
+        ftr_txt8: "Біз батыл идеялар мен олардың әлемдік деңгейдегі цифрлық іске асыру арасындағы алшақтықты өтеміз.",
+        ftr_txt9: "пайдалану шарттары",
+        ftr_txt10: "Байланыс",
+        ftr_txt11: "© Zaroq 2025. Барлық құқықтар қорғалған.",
+        // WMD_Section ===========
+        wmd_txt1: "Бізді ерекшелендіретін нәрсе",
+        wmd_txt2: "Аутсорсинг жоқ",
+        wmd_txt3: "Белгісіз мерзімдер жоқ",
+        wmd_txt4: "“Үлгі” жұмысы жоқ",
+        wmd_txt5: "әлемдік деңгейдегі даму",
+        // story_Section---------
+        stry_txt1: "Біздің әңгіме",
+        stry_txt2: "Zaroq Ұлыбританияда бір миссиямен құрылды:",
+        stry_txt3: "әлемдік деңгейдегі цифрлық өнімдерді ымырасыз жеткізіңіз.",
+        stry_txt4: "“Біздің команданың әрбір мүшесі Біріккен Корольдікте орналасқан және біз қазір бүкіл Қазақстан бойынша өршіл стартаптар мен бизнеске өз қызметтерімізді ұсынуды мақтан тұтамыз.",
+
+        Example: "Қазақша",
+    }
+};
+
+const selector = document.getElementById("languageSelector");
+
+function updateLanguage(lang) {
+    // Save selected language
+    localStorage.setItem("selectedLanguage", lang);
+
+    // Update text content
+    document.getElementById("header1").textContent = translations[lang].header1;
+    document.getElementById("header2").textContent = translations[lang].header2;
+    document.getElementById("header3").textContent = translations[lang].header3;
+    document.getElementById("header4").textContent = translations[lang].header4;
+    document.getElementById("header5").textContent = translations[lang].header5;
+    document.getElementById("header6").textContent = translations[lang].header6;
+    document.getElementById("header7").textContent = translations[lang].header7;
+    document.getElementById("header8").textContent = translations[lang].header8;
+    document.getElementById("header9").textContent = translations[lang].header9;
+    // Hero_Section -------------
+    document.getElementById("hero1").textContent = translations[lang].hero1;
+    document.getElementById("ab_hero1").textContent = translations[lang].ab_hero1;
+    document.getElementById("hero2").textContent = translations[lang].hero2;
+    document.getElementById("hero3").textContent = translations[lang].hero3;
+    document.getElementById("hero4").textContent = translations[lang].hero4;
+    document.getElementById("hero5").textContent = translations[lang].hero5;
+    // Homw_Section_One -------------
+    document.getElementById("sec1_txt1").textContent = translations[lang].sec1_txt1;
+    document.getElementById("sec1_txt2").textContent = translations[lang].sec1_txt2;
+    document.getElementById("sec1_txt3").textContent = translations[lang].sec1_txt3;
+    document.getElementById("sec1_txt4").textContent = translations[lang].sec1_txt4;
+    document.getElementById("sec1_txt5").textContent = translations[lang].sec1_txt5;
+    document.getElementById("sec1_txt6").textContent = translations[lang].sec1_txt6;
+    // What_build_section---------
+    document.getElementById("sec1_txt7").textContent = translations[lang].sec1_txt7;
+    document.getElementById("sec1_txt8").textContent = translations[lang].sec1_txt8;
+    document.getElementById("sec1_txt9").textContent = translations[lang].sec1_txt9;
+    document.getElementById("sec1_txt10").textContent = translations[lang].sec1_txt10;
+    document.getElementById("sec1_txt11").textContent = translations[lang].sec1_txt11;
+    // Home_Section_Two---------
+    document.getElementById("sec2_txt1").textContent = translations[lang].sec2_txt1;
+    document.getElementById("sec2_txt2").textContent = translations[lang].sec2_txt2;
+    document.getElementById("sec2_txt3").textContent = translations[lang].sec2_txt3;
+    document.getElementById("sec2_txt4").textContent = translations[lang].sec2_txt4;
+    document.getElementById("sec2_txt5").textContent = translations[lang].sec2_txt5;
+    document.getElementById("sec2_txt6").textContent = translations[lang].sec2_txt6;
+    document.getElementById("sec2_txt7").textContent = translations[lang].sec2_txt7;
+    document.getElementById("sec2_txt8").textContent = translations[lang].sec2_txt8;
+    document.getElementById("sec2_txt9").textContent = translations[lang].sec2_txt9;
+    document.getElementById("sec2_txt10").textContent = translations[lang].sec2_txt10;
+    // Home_Section_Three---------
+    document.getElementById("sec3_txt1").textContent = translations[lang].sec3_txt1;
+    document.getElementById("sec3_txt2").textContent = translations[lang].sec3_txt2;
+    // Clients_Section---------
+    document.getElementById("cli_txt1").textContent = translations[lang].cli_txt1;
+    document.getElementById("cli_txt2").textContent = translations[lang].cli_txt2;
+    document.getElementById("cli_txt3").textContent = translations[lang].cli_txt3;
+    document.getElementById("cli_txt4").textContent = translations[lang].cli_txt4;
+    document.getElementById("cli_txt5").textContent = translations[lang].cli_txt5;
+    // Project_Section---------
+    document.getElementById("pro_txt1").textContent = translations[lang].pro_txt1;
+    document.getElementById("pro_txt2").textContent = translations[lang].pro_txt2;
+    document.getElementById("pro_txt3").textContent = translations[lang].pro_txt3;
+    // Footer_Section---------
+    document.getElementById("ftr_txt1").textContent = translations[lang].ftr_txt1;
+    document.getElementById("ftr_txt2").textContent = translations[lang].ftr_txt2;
+    document.getElementById("ftr_txt3").textContent = translations[lang].ftr_txt3;
+    document.getElementById("ftr_txt4").textContent = translations[lang].ftr_txt4;
+    document.getElementById("ftr_txt5").textContent = translations[lang].ftr_txt5;
+    document.getElementById("ftr_txt6").textContent = translations[lang].ftr_txt6; document.getElementById("ftr_txt7").textContent = translations[lang].ftr_txt7;
+    document.getElementById("ftr_txt8").textContent = translations[lang].ftr_txt8;
+    document.getElementById("ftr_txt9").textContent = translations[lang].ftr_txt9; document.getElementById("ftr_txt10").textContent = translations[lang].ftr_txt10;
+    document.getElementById("ftr_txt11").textContent = translations[lang].ftr_txt11;
+    // WMD_Section---------
+    document.getElementById("wmd_txt1").textContent = translations[lang].wmd_txt1;
+    document.getElementById("wmd_txt2").textContent = translations[lang].wmd_txt2;
+    document.getElementById("wmd_txt3").textContent = translations[lang].wmd_txt3;
+    document.getElementById("wmd_txt4").textContent = translations[lang].wmd_txt4;
+    document.getElementById("wmd_txt5").textContent = translations[lang].wmd_txt5;
+    // story_Section---------
+    document.getElementById("stry_txt1").textContent = translations[lang].stry_txt1;
+    document.getElementById("stry_txt2").textContent = translations[lang].stry_txt2;
+    document.getElementById("stry_txt3").textContent = translations[lang].stry_txt3;
+    document.getElementById("stry_txt4").textContent = translations[lang].stry_txt4;
+
+
+
+    document.getElementById("Example").textContent = translations[lang].Example;
+}
+
+// On language change
+selector.addEventListener("change", function () {
+    updateLanguage(this.value);
+});
+
+// On page load: check localStorage
+const savedLanguage = localStorage.getItem("selectedLanguage") || "ru";
+selector.value = savedLanguage;
+updateLanguage(savedLanguage);
+
+
+
+
+
